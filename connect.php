ddaef806b5ad8f7169288a7a7117b0176a601841
ojogbon <40061054+ojogbon@users.noreply.github.com>
2021-02-17 12:51:37 -0800

    set heroku db config

--- connect.php
+++ connect.php
@@ -1,9 +1,12 @@
 <?php
 ob_start();
-$host = "localhost";
-$user = "root";
- $password ="";
- $database ="rate";
+
+$devType = "stage";
+$host = ($devType === "dev") ? "localhost" : "us-cdbr-east-03.cleardb.com";
+$user = ($devType === "dev") ? "root" : "b3e7acdd7f8b20" ;
+ $password = ($devType === "dev") ? "" : "0f4536ab" ;
+ $database = ($devType === "dev") ? "rate" :  "heroku_460c654d415c0ef";
+ 
 $mycon=mysqli_connect($host,$user,$password);
 if (!$mycon){
     echo 'error';
